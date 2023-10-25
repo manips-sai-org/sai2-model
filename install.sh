22225d6b076104f0036fe1d7f758153ea2348a95
Mikael Jorda <mikael.jorda@gmail.com>
2023-10-25 13:26:50 +0200

    fixed linker errors on tests

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 # script to install sai2-model
 
 cd rbdl
-mkdir build
-cd build
-cmake -DRBDL_BUILD_ADDON_URDFREADER=ON -DCMAKE_BUILD_TYPE=Release .. && make -j8
+mkdir -p build && cd build && cmake .. && make -j8
 
 cd ../..
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. && make -j8
+mkdir -p build && cd build && cmake .. && make -j8
 
 cd ..
